37d04c7a624aaa6b5ac5a999846a9fa324423ebc
MarianHabor <mariohabor@gmail.com>
2025-02-10 21:35:54 +0000

    imlp ClCache struct's only

--- src/operations/cl_sync_cache.rs
+++ src/operations/cl_sync_cache.rs
@@ -1,10 +1,30 @@
 use crate::operations::sys_ops;
-use tokio::fs;
 
-pub struct ClCache {}
+use anyhow::Result;
+use chrono::{DateTime, Local};
+use hashbrown::HashMap;
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+// This is the bincode file that gets loaded in to memory
+pub struct ClCache {
+    pub data: Arc<Mutex<HashMap<String, ToVery>>>,
+    pub cache_storage_path: String,
+}
+
+// This is each individual representation of files
+// that need to be checked and uploaded
+// file_path: where the file or dir lives
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ToVery {
+    pub file_path: String,
+    pub last_saved: DateTime<Local>,
+}
 
 impl ClCache {
-    pub async fn new() -> Self {
+    pub async fn new() -> Result<Self> {
+        let _ = sys_ops::config_dir_exists().await;
         todo!()
     }
 }
